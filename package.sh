7860e258dfccbbf77dd5c27530079befd7b2ad39
Jakob Russel <jaru@clinical-microbiomics.com>
2023-08-24 10:46:12 +0200

    Update version workflow

--- package.sh
+++ package.sh
@@ -1,4 +1,8 @@
+# New version
+## 1) Update version in setup.py and commit and push
+## 2) Pull request of dev into main
+## 3) Make release on GitHub
+## 4) Run this code:
 rm -r maginator.egg-info/ dist/ build/
 python setup.py sdist
-python setup.py install
 twine upload dist/*
